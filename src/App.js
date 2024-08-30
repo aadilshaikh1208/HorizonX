fb5f0d1da59c956eff0a3b720c54f048f087320e
aadilshaikh1208 <akhtarhusain3385@gmail.com>
2024-08-30 21:44:36 +0530

    Updated Discover Section and new Added Trend and LoanSection

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './Components/Navbar/Navbar';
 import HeroSection from './Components/HeroSection/HeroSection';
 import HomeSection from './Components/HomeSection/HomeSection';
 import DiscoverSection from './Components/DiscoverSection/DiscoverSection';
+import TrendsSection from './Components/TrendSection/TrendSection';
+import LoanSection from './Components/LoanSection/LoanSection';
 function App() {
     return (
         <div className="App">
@@ -10,6 +12,8 @@ function App() {
             <HeroSection/>
             <HomeSection/>
             <DiscoverSection />
+            <TrendsSection />
+            <LoanSection />
             {/* Other components like pages will go here */}
         </div>
     );
